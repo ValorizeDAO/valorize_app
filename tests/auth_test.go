8b43b7d9e8c5b641c710f93a00655cf2621d22c1
Javier Gonzalez <javier.e.glz@gmail.com>
2021-07-20 20:51:48 -0500

    Add accounts [ch17]: cleanup

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -1,7 +1,6 @@
 package tests
 
 import (
-	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
@@ -29,32 +28,28 @@ func mockDb() (*gorm.DB, sqlmock.Sqlmock) {
 
 func TestLogin(t *testing.T) {
 	// Setup
-	e := echo.New()
-	//requestBody := "username=javier123454321&password=test"
 	form := url.Values{}
 	form.Add("username", "javier123454321")
 	form.Add("password", "test")
 	req := httptest.NewRequest("POST", "/login", strings.NewReader(form.Encode()))
 	req.Form = form
-	//req := httptest.NewRequest(http.MethodPost, "/login", strings.NewReader(requestBody))
-	//req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	fmt.Println(req.FormValue("username"))
-	fmt.Println(req.FormValue("password"))
 	rec := httptest.NewRecorder()
+
+	e := echo.New()
 	c := e.NewContext(req, rec)
 
 	fakeDb, mock := mockDb()
 	mock.MatchExpectationsInOrder(false)
 	mock.ExpectBegin()
 	columns := []string{"id", "username", "email", "password", "deleted_at"}
-	fmt.Print(len(columns))
+
 	mock.ExpectQuery(regexp.QuoteMeta(
 		"SELECT * FROM `users`")).
 		WillReturnRows(sqlmock.NewRows(columns).
 			AddRow("4", "javier123454321", "javier@example.com", "$2a$10$e.kv61mInrLwSwD5YUIoLerxI3FEzh5w.PAmXXrk3.RDuozLRoTv2", nil))
 
 	auth := handlers.AuthHandler{DB: fakeDb}
-	// Assertions
+
 	if assert.NoError(t, auth.Login(c), "/login fails") {
 		assert.Equal(t, http.StatusCreated, rec.Code)
 	}
