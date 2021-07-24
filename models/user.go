c27a8ee314f4aff33f1a0d493c9fa8f6715293d2
Javier Gonzalez <javier.e.glz@gmail.com>
2021-07-24 12:05:35 -0500

    Integrate Stripe [ch79]: add payment accepted and user handler

--- models/user.go
+++ models/user.go
@@ -10,3 +10,36 @@ type User struct {
 	Password string `json:"password" gorm:"type:varchar(200);"`
 	Wallets  []Wallet
 }
+
+func GetUserByID(id uint, db gorm.DB) (*User, error) {
+	var m User
+	if err := db.First(&m, id).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &m, nil
+}
+
+func GetUserByEmail(e string, db gorm.DB) (*User, error) {
+	var m User
+	if err := db.Where(&User{Email: e}).First(&m).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &m, nil
+}
+
+func GetUserByUsername(username string, db gorm.DB) (*User, error) {
+	var m User
+	if err := db.Where(&User{Username: username}).First(&m).Error; err != nil {
+		if gorm.IsRecordNotFoundError(err) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &m, nil
+}
