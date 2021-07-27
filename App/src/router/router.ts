450eb5516d2e332f258e0573878ee742d62e1fec
Javier Gonzalez <javier.e.glz@gmail.com>
2021-07-26 23:05:43 -0500

    Add login [ch22]: begin auth guards

--- App/src/router/router.ts
+++ App/src/router/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import { isAuthenticated } from "../services/authentication";
 
 const routes = [
   {
@@ -18,4 +19,16 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  const publicRoutes = ["Login", "Register"];
+  let { name } = to;
+  name = name?.toString() || "";
+  if (!publicRoutes.includes(name) && !isAuthenticated) next({ name: "Login" });
+  else {
+    console.log("going to next");
+    next();
+  }
+});
+
 export default router;
