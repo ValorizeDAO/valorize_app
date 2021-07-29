262700d4d2650584944e4e52da188eaf2e029f66
Javier Gonzalez <javier.e.glz@gmail.com>
2021-07-29 18:58:30 -0500

    Add login [ch22]: make credentials persist through requests

--- App/src/vuex/vuex.ts
+++ App/src/vuex/vuex.ts
@@ -1,15 +1,39 @@
 import { createStore } from "vuex"
+import { User } from "../models/user"
 
-// Create a new store instance.
 export default createStore({
   state() {
     return {
+      checkingAuth: false,
       authenticated: false,
+      user: {
+        id: "",
+        email: "",
+        name: "",
+        username: "",
+      },
     }
   },
+  getters: {
+    authenticated: (state: State) => state.authenticated,
+    user: (state: State) => state.user,
+    checkingAuth: (state: State) => state.checkingAuth,
+  },
   mutations: {
-    authenticated(state: { authenticated: boolean }, payload: boolean) {
+    authenticated(state: State, payload: boolean) {
+      state.checkingAuth = false
       state.authenticated = payload
+    },
+    setUser(state:State, payload: User) {
+      state.authenticated = true // assumes setUser is only called by logging in
+      state.checkingAuth = false
+      state.user = payload
     },
   },
 })
+
+interface State {
+  checkingAuth: boolean,
+  authenticated: boolean,
+  user: User
+}
