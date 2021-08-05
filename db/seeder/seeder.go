f55f9f559d7a3517a61c68b57b72e85fb0c55697
Javier Gonzalez <javier.e.glz@gmail.com>
2021-08-04 22:48:40 -0500

    Deployed page [ch25]: add comma

--- db/seeder/seeder.go
+++ db/seeder/seeder.go
@@ -1,46 +1,46 @@
 package seeder
 
 import (
-  "fmt"
-  "github.com/jinzhu/gorm"
-  "golang.org/x/crypto/bcrypt"
-  "valorize-app/models"
-  "valorize-app/services/ethereum"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+	"valorize-app/models"
+	"valorize-app/services/ethereum"
 )
 
-func RunSeeder(db *gorm.DB) error{
-  user, err := UserSeeder(db, "javier123454321", "test@test.com", "test", true)
-  UserSeeder(db, "zselyke", "test@test2.com", "test", false)
-  creatorToken := models.Token{
-    UserId:          user.ID,
-    ContractVersion: "v0.0.1",
-    Name:            "JAVICoin",
-    Symbol:          "JABA",
-    Network:         "MAINNET",
-    OwnerAddress:    "0x810aafa05789dd461f81b22de31a2b21b2fbe3be",
-    Address:         "0x05F3074138b9bfAbe8ADf0f68f2AA33047a8192e",
-    TxHash:          "0xb57e816e5ca3c68d7d9d7522380318e8fde8dd320a247ac15997c3cb0956553f",
-  }
-  err = db.Create(&creatorToken).Error
-  return err
+func RunSeeder(db *gorm.DB) error {
+	user, err := UserSeeder(db, "javier123454321", "test@test.com", "test", true)
+	_, err = UserSeeder(db, "zselyke", "test@test2.com", "test", false)
+	creatorToken := models.Token{
+		UserId:          user.ID,
+		ContractVersion: "v0.0.1",
+		Name:            "JAVICoin",
+		Symbol:          "JABA",
+		Network:         "MAINNET",
+		OwnerAddress:    "0x810aafa05789dd461f81b22de31a2b21b2fbe3be",
+		Address:         "0x05F3074138b9bfAbe8ADf0f68f2AA33047a8192e",
+		TxHash:          "0xb57e816e5ca3c68d7d9d7522380318e8fde8dd320a247ac15997c3cb0956553f",
+	}
+	err = db.Create(&creatorToken).Error
+	return err
 }
 
 func UserSeeder(db *gorm.DB, username string, email string, password string, hasDeployed bool) (models.User, error) {
-  hash, _ := bcrypt.GenerateFromPassword([]byte(password), 10)
-  user := models.User{
-    Email:    email,
-    Username: username,
-    Password: string(hash),
-    HasDeployedToken: hasDeployed
-  }
-  err := db.Create(&user).Error
-  if err != nil {
-    return models.User{}, err
-  }
-  _, err = ethereum.StoreUserKeystore(password, user.ID, db)
-  if err != nil {
-    fmt.Println("error: " + err.Error())
-    return models.User{}, err
-  }
-  return user, err
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), 10)
+	user := models.User{
+		Email:            email,
+		Username:         username,
+		Password:         string(hash),
+		HasDeployedToken: hasDeployed,
+	}
+	err := db.Create(&user).Error
+	if err != nil {
+		return models.User{}, err
+	}
+	_, err = ethereum.StoreUserKeystore(password, user.ID, db)
+	if err != nil {
+		fmt.Println("error: " + err.Error())
+		return models.User{}, err
+	}
+	return user, err
 }
