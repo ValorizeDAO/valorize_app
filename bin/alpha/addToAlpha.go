d58caff614185d7598fc7b634ec34ed140426a8e
Javier Gonzalez <javier.e.glz@gmail.com>
2021-10-04 21:37:44 -0500

    [sc-152]: make command line to add user to alpha

---
+++ bin/alpha/addToAlpha.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"valorize-app/config"
+	"valorize-app/db"
+	"valorize-app/models"
+
+	"github.com/jinzhu/gorm"
+)
+
+func main() {
+	cfg := config.NewConfig()
+	database := db.Init(cfg)
+	defer database.Close()
+	// send username parameter to command line
+	username := os.Args[1]
+	fmt.Print(os.Args)
+	user, err := AddToAlpha(database, username)
+	if err == nil {
+		fmt.Print("Added to alpha ", user.Username)
+	} 
+
+}
+
+func AddToAlpha(db *gorm.DB, username string) (models.User, error) {
+	userData, err := models.GetUserByUsername(username, *db)
+
+	if err != nil {
+		fmt.Println("could not find user " + username + err.Error())
+		return models.User{}, err
+	}
+	userData.IsAlphaUser = true
+
+	err = db.Save(&userData).Error
+	if err != nil {
+		fmt.Println(err.Error())
+		return models.User{}, err
+	}
+	return userData, nil
+}
+
+
