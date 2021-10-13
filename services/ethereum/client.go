44dafd1929ff24cb87d4692f9eaf0adc5502f11b
Javier Gonzalez <javier.e.glz@gmail.com>
2021-10-12 21:19:35 -0500

    fix navbar

--- services/ethereum/client.go
+++ services/ethereum/client.go
@@ -1,6 +1,7 @@
 package ethereum
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -24,12 +25,12 @@ import (
 )
 
 func Connect() (*ethclient.Client, error) {
-	client, err := ethclient.Dial(os.Getenv("ETH_NODE"))
+	client, err := ethclient.Dial(os.Getenv("TESTNET_NODE"))
 
 	if err != nil {
 		log.Println(err)
 	} else {
-		fmt.Println("=======================\n\nConnected to ethereum\n\n=======================")
+		fmt.Println("=======================\n\nConnected to " + os.Getenv("TESTNET") +"\n\n=======================")
 	}
 	return client, err
 }
@@ -39,14 +40,14 @@ func MainnetConnection() (*ethclient.Client, error) {
 	if os.Getenv("ENVIRONMENT") == "PRODUCTION" {
 		clientUrl = os.Getenv("MAINNET_NODE")
 	} else {
-		clientUrl = os.Getenv("ETH_NODE")
+		clientUrl = os.Getenv("TESTNET_NODE")
 	}
 	client, err := ethclient.Dial(clientUrl)
 
 	if err != nil {
 		log.Println(err.Error())
 	} else {
-		fmt.Println("=======================\n\nConnected to ethereum mainnet\n\n=======================")
+		fmt.Println("=======================\n\nConnected to ethereum " + os.Getenv("MAINNET") +"\n\n=======================")
 	}
 	return client, err
 }
@@ -93,7 +94,7 @@ func LaunchContract(client *ethclient.Client, name string, ticker string, networ
 	hotWalletBlob := []byte(os.Getenv("HOTWALLET_KEYSTORE"))
 	hotWallet, err := keystore.DecryptKey(hotWalletBlob, hotWalletPass)
 
-	fmt.Print(network + "_ID", os.Getenv(network+"_ID"))
+	fmt.Println(network + "_ID:", os.Getenv(network+"_ID"))
 	_check(err)
 	gasPrice, err := GetGasPrice(network)
 	_check(err)
@@ -102,9 +103,18 @@ func LaunchContract(client *ethclient.Client, name string, ticker string, networ
 	txOptions.Value = big.NewInt(0)      // in wei
 	txOptions.GasLimit = uint64(3000000) // in units
 	txOptions.GasPrice = big.NewInt(gasPrice)
-	fmt.Printf("Gas Price: %v", gasPrice)
+	fmt.Printf("Gas Price: %v\n", gasPrice)
 	n := new(big.Int)
 	initialAmount, _ := n.SetString("1000000000000000000000", 10)
+
+	//see if balance is enough to launch
+	balance, err := client.BalanceAt(context.Background(), hotWallet.Address, nil)
+	fmt.Println("Balance: ", balance)
+	gasCost := big.NewInt(0).Mul(txOptions.GasPrice, big.NewInt(3000000))
+	fmt.Println("Gas Cost: ", gasCost)
+	if balance.Cmp(gasCost) == -1 {
+		return common.Address{}, nil, nil, errors.New("Not enough balance to launch contract")
+	}
 	
 	address, tx, token, err := contracts.DeployCreatorToken(txOptions, client, initialAmount, name, ticker)
 	if err != nil {
