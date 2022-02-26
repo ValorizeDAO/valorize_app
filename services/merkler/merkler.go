e1ea4a29076fa43682478f8f19f75a53da8503bb
Javier Gonzalez <javier.e.glz@gmail.com>
2022-02-26 15:37:47 -0600

    WIP make node hashing

--- services/merkler/merkler.go
+++ services/merkler/merkler.go
@@ -27,9 +27,66 @@ type ContentData struct {
 // 	return hex.EncodeToString(thisHash) == hex.EncodeToString(otherHash), nil
 // }
 
-func GenerateAirdropMerkleRoot(addressToClaimAmountMap [][]string) ([]byte, error) {
 
-	var leaves [][]byte
+type Node struct {
+	Left 		Hashable
+	Right 		Hashable
+	LeftBytes 	[]byte
+	RightBytes 	[]byte
+}
+type Leaf struct {
+	Address string
+	ClaimAmount *big.Int
+}
+type BaseHashable struct {
+	BaseBytes []byte
+}
+
+func (base BaseHashable) Hash() []byte {
+	fmt.Println("Hashing base bytes")
+	return solsha3.SoliditySHA3(
+	   solsha3.Bytes32(base.BaseBytes),
+   )
+}
+
+func (leaf Leaf) Hash() []byte {
+	hash := solsha3.SoliditySHA3(
+		solsha3.Address(leaf.Address),
+		solsha3.Uint256(leaf.ClaimAmount),
+	)
+	return hash
+}
+
+func (leaf Leaf) ToHashableBytes() BaseHashable {
+	return BaseHashable{BaseBytes: leaf.Hash()}
+}
+
+type Hash [20]byte
+
+type Hashable interface {
+	Hash() []byte
+}
+
+func (n Node) Hash() []byte {
+	hash := solsha3.SoliditySHA3(
+		solsha3.Bytes32(n.LeftBytes),
+		solsha3.Bytes32(n.RightBytes),
+	)
+	fmt.Printf("\n==\nnode(inside Hash()): \nleft: %v\nright %v\n hash: %v\n==\n", 
+		hex.EncodeToString(n.LeftBytes), 
+		hex.EncodeToString(n.RightBytes), 
+		hex.EncodeToString(hash),
+	)
+ 	return hash
+}
+type EmptyNode []byte
+
+func (e EmptyNode) Hash() []byte {
+	return []byte{}
+}
+
+func GenerateAirdropMerkleRoot(addressToClaimAmountMap [][]string) ([]byte, error) {
+	var leaves []Leaf
 	for i := 0; i < len(addressToClaimAmountMap); i++ {
 		address := addressToClaimAmountMap[i][0]
 		claimString := addressToClaimAmountMap[i][1]
@@ -37,124 +94,53 @@ func GenerateAirdropMerkleRoot(addressToClaimAmountMap [][]string) ([]byte, erro
 		if !ok || !ethereum.CheckAddress(address) {
 			return []byte(""), errors.New("invalid value in input for address: " + address + ", claimAmount: " + claimString + "in index: " + strconv.Itoa(i))
 		}
-
-		hash := solsha3.SoliditySHA3(
-			solsha3.Address(address),
-			solsha3.Uint256(claimAmount),
-		)
-
-		leaves = append(leaves, hash)
-		
+		leaves = append(leaves, Leaf{Address: address, ClaimAmount: claimAmount})
 	}
 	sort.SliceStable(leaves, func(i, j int) bool {
-		return bytes.Compare(leaves[i], leaves[j]) < 0
+		return bytes.Compare(leaves[i].Hash(), leaves[j].Hash()) < 0
 	})
-	// var contents []merkletree.Content
-	// for i := 0; i < len(leaves); i++ {
-	// 	contents = append(contents, ContentData{leaves[i]})
-	// }
-	// var hashes []string
-	// for i := 0; i < len(contents); i++ {
-	// 	content, _ := contents[i].CalculateHash()
-	// 	hashes = append(hashes, "0x" + hex.EncodeToString(content) + ",\n")
-	// }
-	_ = NewMerkleTree(leaves, solsha3.Bytes32)
-	// fmt.Println(hashes)
-	// if err != nil {
-	// 	return []byte(""), err
-	// }
-	// mr := t.MerkleRoot()
+	_ = NewMerkleTree(leaves)
 	return []byte{}, nil
 }
 
-func NewMerkleTree(leaves [][]byte, hashing func(interface{}) []byte) error {
-	tree := makeTree(leaves)
-	for _, leaf := range tree {
-		fmt.Printf("leaf: \n%v\n", hex.EncodeToString(leaf))
-	}
+func NewMerkleTree(leaves []Leaf) error {
+	_ = turnLeavesIntoNodes(leaves)
 	return nil
 }
 
-func makeTree(leaves [][]byte) [][]byte {
-	var branches [][]byte
+func turnLeavesIntoNodes(leaves []Leaf) []Node {
+	var nodes []Node
 	for i := 0; i < len(leaves); i = i + 2 {
-		if (i + 1 == len(leaves)) {
-			return append(branches, leaves[i])
-		} else {
-			left := leaves[i]
-			right := leaves[i+1]
-			hash := solsha3.SoliditySHA3(
-				solsha3.Bytes32(left),
-				solsha3.Bytes32(right),
-			)
-			fmt.Printf("Hashing: \n 0x%v w/ 0x%v\n result: \n0x%v\n\n",
-			 hex.EncodeToString(left),
-			 hex.EncodeToString(right),
-			 hex.EncodeToString(hash),
-			)
-			branches = append(branches, hash)
-			sort.SliceStable(branches, func(i, j int) bool {
-				return bytes.Compare(branches[i], branches[j]) < 0
+		if i + 1 < len(leaves) {
+			nodes = append(nodes, Node{
+				Left: leaves[i].ToHashableBytes(), 
+				Right: leaves[i+1].ToHashableBytes(),
+				LeftBytes: leaves[i].Hash(),
+				RightBytes: leaves[i+1].Hash(),
 			})
+		} else {
+			nodes = append(nodes, Node{Left: leaves[i].ToHashableBytes(), Right: EmptyNode{}, LeftBytes: leaves[i].Hash()})
 		}
 	}
-	
-	if len(branches[len(branches)-1]) != 1 {
-		branches = makeTree(branches)
-	}
-	return branches
+	return nodes
 }
-// _createHashes(nodes) {
-// 	while (nodes.length > 1) {
-// 		const layerIndex = this.layers.length;
-// 		this.layers.push([]);
-// 		for (let i = 0; i < nodes.length; i += 2) {
-// 			if (i + 1 === nodes.length) {
-// 				if (nodes.length % 2 === 1) {
-// 					let data = nodes[nodes.length - 1];
-// 					let hash = data;
-// 					// is bitcoin tree
-// 					if (this.isBitcoinTree) {
-// 						// Bitcoin method of duplicating the odd ending nodes
-// 						data = Buffer.concat([buffer_reverse_1.default(data), buffer_reverse_1.default(data)]);
-// 						hash = this.hashFn(data);
-// 						hash = buffer_reverse_1.default(this.hashFn(hash));
-// 						this.layers[layerIndex].push(hash);
-// 						continue;
-// 					}
-// 					else {
-// 						if (this.duplicateOdd) {
-// 							// continue with creating layer
-// 						}
-// 						else {
-// 							// push copy of hash and continue iteration
-// 							this.layers[layerIndex].push(nodes[i]);
-// 							continue;
-// 						}
-// 					}
-// 				}
-// 			}
-// 			const left = nodes[i];
-// 			const right = i + 1 === nodes.length ? left : nodes[i + 1];
-// 			let data = null;
-// 			let combined = null;
-// 			if (this.isBitcoinTree) {
-// 				combined = [buffer_reverse_1.default(left), buffer_reverse_1.default(right)];
-// 			}
-// 			else {
-// 				combined = [left, right];
-// 			}
-// 			if (this.sortPairs) {
-// 				combined.sort(Buffer.compare);
-// 			}
-// 			data = Buffer.concat(combined);
-// 			let hash = this.hashFn(data);
-// 			// double hash if bitcoin tree
-// 			if (this.isBitcoinTree) {
-// 				hash = buffer_reverse_1.default(this.hashFn(hash));
-// 			}
-// 			this.layers[layerIndex].push(hash);
+
+
+// func makeTree(base []Hashable) []Hashable {
+// 	var nodes []Hashable
+// 	for i := 0; i < len(base); i = i + 2 {
+// 		if i + 1 < len(base) {
+// 			nodes = append(nodes, Node{Left: base[i], Right: base[i+1]})
+// 		} else {
+// 			nodes = append(nodes, Node{Left: base[i], Right: EmptyNode{}})
 // 		}
-// 		nodes = this.layers[layerIndex];
+// 		sort.SliceStable(nodes, func(i, j int) bool {
+// 			return bytes.Compare(nodes[i].Hash(), nodes[j].Hash()) < 0
+// 		})
+// 	}
+// 	if len(nodes) == 1 {
+// 		return nodes
+// 	} else {
+// 		return makeTree(nodes)
 // 	}
 // }
